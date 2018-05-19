a9a34bf772ec7ad980eed92b67ceca4858b24716
Pedro Tôrres <t0rr3sp3dr0@gmail.com>
2018-05-19 16:59:14 -0300

    Initial commit

---
+++ src/main/java/br/ufpe/cin/if688/minijava/antlr/ExprBaseVisitor.java
@@ -0,0 +1,28 @@
+// Generated from /Users/pedro/IdeaProjects/if688/src/main/resources/Expr.g4 by ANTLR 4.7
+package br.ufpe.cin.if688.minijava.antlr;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link ExprVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public class ExprBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements ExprVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitProg(ExprParser.ProgContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitExpr(ExprParser.ExprContext ctx) { return visitChildren(ctx); }
+}
